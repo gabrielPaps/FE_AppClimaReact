129088c5a4a2a0755fd5cef5de1a52777891c2ef
Gabriel Caruso <carusogabriel9@gmail.com>
2020-06-30 17:47:02 -0300

    Terminé

--- src/app/App.js
+++ src/app/App.js
@@ -23,21 +23,26 @@ class App extends Component{
         const cityValue = city.value;
         const countryValue = country.value;
 
-        const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}&units=metric`;
-        const response = await fetch(API_URL);
-        const data = await response.json();
-        console.log(data);
+        if (cityValue && countryValue){
 
-        this.setState({
-            temperature: data.main.temp,
-            description: data.weather[0].description,
-            humidity: data.main.humidity,
-            wind_speed: data.wind.speed,
-            city: data.name,
-            country: data.sys.country,
-            error: null
+            const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}&units=metric`;
+            const response = await fetch(API_URL);
+            const data = await response.json();
+            console.log(data);
 
-        });
+            this.setState({
+                temperature: data.main.temp,
+                description: data.weather[0].description,
+                humidity: data.main.humidity,
+                wind_speed: data.wind.speed,
+                city: data.name,
+                country: data.sys.country,
+                error: null
+
+            });
+        } else {
+            this.setState({error: 'Ingrese una locación válida'})
+        }
 
     }
     render(){
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -3,7 +3,37 @@ import React from 'react';
 const WeatherInfo = props => {
     return(
         <div>
-            Weather Info
+            {
+                props.error &&
+                <div className="alert alert-danger">
+                    <p>{props.error}</p>
+                </div>
+            }
+            {
+                props.temperature ? 
+
+                <div className="card card-body">
+                    <p>
+                        Lugar: {props.city}, {props.country}
+                    </p>
+                    <p>
+                        Clima:  {props.description}
+                    </p>
+                    <p>
+                        Temperatura: {props.temperature} °C,
+                    </p>
+                    <p>
+                        Humedad: {props.humidity}
+                    </p>
+                    <p>
+                        Velocidad del Viento: {props.wind_speed}
+                    </p>
+            </div>
+            :
+            <div className="card card-body"></div>
+            }
+
+            
         </div>
     )
 }
