871c94482cdb1d5d74b7abee0fd502ba4e249ef2
Gabriel Caruso <carusogabriel9@gmail.com>
2020-06-30 12:42:49 -0300

    Primer Load

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
+
+module.exports = {
+    entry: [
+        './src/app/index.js'
+    ],
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader:'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        })
+    ]
+};
